8a0d1cdbaf6537c0bf2f0dcf7afd57f97d4dfe2b
Bugkodc <145227290+bugkodc@users.noreply.github.com>
2023-11-19 22:16:51 +0700

    'hoanthanh_RC4'

--- Assets/Script/Managerinput.cs
+++ Assets/Script/Managerinput.cs
@@ -25,12 +25,11 @@ public class Managerinput : MonoBehaviour
     [SerializeField] public Button _EncryptButton;
     [SerializeField] public Button _DecryptButton;
     [SerializeField] public Button _confirmButton;
-    private byte[] key;
-    private byte[] fileData;
+    private string _key;
+    private string _thongDiep;
     #region start
     private void Start()
     {
-        _confirmButton.onClick.AddListener(OnConfirmButtonClick);
         _RandomKey_Button.onClick.AddListener(GenerateRandomKey);
         _SelectFile_Button.onClick.AddListener(SelectFile);
         _EncryptButton.onClick.AddListener(EncryptFile);
@@ -55,135 +54,92 @@ public class Managerinput : MonoBehaviour
     #region ramdomkey
    private void GenerateRandomKey()
    {
-        key = new byte[16]; // Key length in bytes
-        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        _key = GenerateRandomKeyOfLength(16); // Đặt độ dài key tùy ý, ở đây là 16
+        _Key_Input.text = _key;
+    }
+    private string GenerateRandomKeyOfLength(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        string key = "";
+        for (int i = 0; i < length; i++)
         {
-            rng.GetBytes(key);
+            key += chars[UnityEngine.Random.Range(0, chars.Length)];
         }
-        string keyHex = BitConverter.ToString(key).Replace("-", string.Empty);
-        string keyString = Convert.ToBase64String(Encoding.UTF8.GetBytes(keyHex));
-        _Key_Input.text = keyString;
+        return key;
     }
     #endregion
     #region EncryptFile
     private void EncryptFile()
     {
-        if (_Key_Input.text == null || key.Length != 16)
-        {
-            _ThongDiep_Output.text = "Please generate a key first!";
-            return;
-        }
-        RC4 rc4 = new RC4(key);
-        byte[] encryptedData = rc4.Encrypt(fileData);
-        string keyString = ConvertByteArrayToString(encryptedData);
-        _ThongDiep_Output.text = keyString;
+        _key = _Key_Input.text;
+        _thongDiep = _ThongDiep_Input.text;
+
+        byte[] encryptedBytes = RC4.Encrypt(Encoding.UTF8.GetBytes(_thongDiep), Encoding.UTF8.GetBytes(_key));
+        string encryptedText = Convert.ToBase64String(encryptedBytes);
+
+        _ThongDiep_Output.text = encryptedText;
     }
     #endregion
     #region DecryptFile
     private void DecryptFile()
     {
-        if (_Key_Input.text == null || key.Length != 16)
-        {
-            _ThongDiep_Output.text = "Please generate a key first!";
-            return;
-        }
-        RC4 rc4 = new RC4(key);
-        byte[] decryptedData = rc4.Decrypt(fileData);
-        string keyString = Encoding.UTF8.GetString(decryptedData);
-        _ThongDiep_Output.text = keyString;
-    }
-    #endregion
-    #region string to byte and byte to string
-    private string ConvertByteArrayToString(byte[] byteArray)
-    {
-        string hexString = Encoding.UTF8.GetString(byteArray);
-        return hexString;
-    }
+        _key = _Key_Input.text;
+        _thongDiep = _ThongDiep_Input.text;
 
-    private byte[] ConvertToByteArray(string hexString)
-    {
-        byte[] byteArray = Encoding.UTF8.GetBytes(hexString);
-        return byteArray;
-    }
-    private bool IsValidHexString(string hexString)
-    {
-        Regex regex = new Regex("^[0-9A-F]+$");
-        return regex.IsMatch(hexString);
-    }
-    #endregion
-    #region xacnhan input
-    private void OnConfirmButtonClick()
-    {
-        string keyString = _Key_Input.text;
-        key = ConvertToByteArray(keyString);
-        string DataString = _ThongDiep_Input.text;
-        fileData = ConvertToByteArray(DataString);
+        byte[] encryptedBytes = Convert.FromBase64String(_thongDiep);
+        byte[] decryptedBytes = RC4.Decrypt(encryptedBytes, Encoding.UTF8.GetBytes(_key));
+        string decryptedText = Encoding.UTF8.GetString(decryptedBytes);
+
+        _ThongDiep_Output.text = decryptedText;
     }
     #endregion
 }
 #region RC4
-public class RC4
+public static class RC4
 {
-    private byte[] S;
-    private byte[] T;
-    private int keyLength;
-
-    public RC4(byte[] key)
+    public static byte[] Encrypt(byte[] data, byte[] key)
     {
-        keyLength = key.Length;
-        S = new byte[256];
-        T = new byte[256];
+        byte[] output = new byte[data.Length];
+        byte[] S = new byte[256];
+        byte[] K = new byte[256];
 
         for (int i = 0; i < 256; i++)
         {
             S[i] = (byte)i;
-            T[i] = key[i % keyLength];
+            K[i] = key[i % key.Length];
         }
 
         int j = 0;
         for (int i = 0; i < 256; i++)
         {
-            j = (j + S[i] + T[i]) % 256;
+            j = (j + S[i] + K[i]) % 256;
             Swap(S, i, j);
         }
-    }
 
-    public byte[] Encrypt(byte[] data)
-    {
-        byte[] encryptedData = new byte[data.Length];
-        int i = 0;
-        int j = 0;
-
-        for (int k = 0; k < data.Length; k++)
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < data.Length; i++)
         {
-            i = (i + 1) % 256;
-            j = (j + S[i]) % 256;
-
-            Swap(S, i, j);
-
-            int t = (S[i] + S[j]) % 256;
-
-            encryptedData[k] = (byte)(data[k] ^ S[t]);
+            x = (x + 1) % 256;
+            y = (y + S[x]) % 256;
+            Swap(S, x, y);
+            int t = (S[x] + S[y]) % 256;
+            output[i] = (byte)(data[i] ^ S[t]);
         }
 
-        return encryptedData;
+        return output;
     }
 
-    public byte[] Decrypt(byte[] encryptedData)
+    public static byte[] Decrypt(byte[] data, byte[] key)
     {
-        // Decrypt the encrypted data using the RC4 algorithm
-        byte[] decryptedData = Encrypt(encryptedData);
-
-        // Convert the decrypted data back to a string using the correct encoding
-        string decryptedString = Encoding.UTF8.GetString(decryptedData);
-        return decryptedString;
+        return Encrypt(data, key);
     }
 
-    private void Swap(byte[] array, int i, int j)
+    private static void Swap(byte[] array, int i, int j)
     {
         byte temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-      }
+    }
 }
 #endregion
