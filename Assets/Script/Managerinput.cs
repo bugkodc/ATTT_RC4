e99546f32787ec7b7f4d585a48e7445e7067a07b
Bugkodc <145227290+bugkodc@users.noreply.github.com>
2023-11-19 21:55:05 +0700

    RC4

--- Assets/Script/Managerinput.cs
+++ Assets/Script/Managerinput.cs
@@ -8,166 +8,182 @@ using UnityEngine.TextCore.Text;
 using System.Text;
 using System;
 using Unity.VisualScripting;
+using UnityEngine.EventSystems;
+using System.IO;
+using System.Security.Cryptography;
+using UnityEngine.Windows;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 public class Managerinput : MonoBehaviour
 {
-    [SerializeField] public TextMeshProUGUI _input_p;
-    [SerializeField] public TextMeshProUGUI _input_q;
-    [SerializeField] public TextMeshProUGUI _input_s;
-    [SerializeField] public TextMeshProUGUI _input_n;
-    [SerializeField] public TextMeshProUGUI _input_e;
-    [SerializeField] public TextMeshProUGUI _input_d;
-    [SerializeField] public TextMeshProUGUI _input_banro;
-    [SerializeField] public TextMeshProUGUI _input_banma;
-    [SerializeField] public TextMeshProUGUI _output_banma;
-    [SerializeField] public TextMeshProUGUI _output_banro;
-    public int _danhDau = 0;
-    private bool nguyenToCungNhau(int ai, int bi)// "Hàm kiểm tra hai số nguyên tố cùng nhau"
+    [SerializeField] private TMP_InputField _Key_Input;
+    [SerializeField] private TMP_InputField _ThongDiep_Input;
+    [SerializeField] private TMP_InputField _ThongDiep_Output;
+    [SerializeField] public Button _RandomKey_Button;
+    [SerializeField] public Button _SelectFile_Button;
+    [SerializeField] public Button _EncryptButton;
+    [SerializeField] public Button _DecryptButton;
+    [SerializeField] public Button _confirmButton;
+    private byte[] key;
+    private byte[] fileData;
+    #region start
+    private void Start()
     {
-        bool ktx_;
-        // giải thuật Euclid;
-        int temp;
-        while (bi != 0)
-        {
-            temp = ai % bi;
-            ai = bi;
-            bi = temp;
-        }
-        if (ai == 1) { ktx_ = true; }
-        else ktx_ = false;
-        return ktx_;
-    }
-    int _input_P, _input_Q, _input_N, _input_E, _intput_D, _intput_S;
-    string _intput_banma , _intput_banro ;
-    public int RSA_d_dau = 0;
-    int _RSA_N, _RSA_SoPhi_N, _RSA_E, _RSA_D;
-    string _str_N, _str_SoPhi_N, _str_E, _str_D, _str_BanMa, _str_BanGiaiMa;
-    #region Input
-    private void Inputsystem()
-    {
-        _input_P = Int32.Parse(_input_p.text);
-        _input_Q = Int32.Parse(_input_q.text);
-        _input_N = Int32.Parse(_input_n.text);
-        _input_E = Int32.Parse(_input_e.text);
-        _intput_D = Int32.Parse(_input_d.text);
-        _intput_S = Int32.Parse(_input_s.text);
-        _intput_banma = _input_banma.ToString();
-        _intput_banro = _input_banro.ToString();
+        _confirmButton.onClick.AddListener(OnConfirmButtonClick);
+        _RandomKey_Button.onClick.AddListener(GenerateRandomKey);
+        _SelectFile_Button.onClick.AddListener(SelectFile);
+        _EncryptButton.onClick.AddListener(EncryptFile);
+        _DecryptButton.onClick.AddListener(DecryptFile);
     }
     #endregion
-    #region random_number
-    private int RSA_ChonSoNgauNhien()
+    #region input file
+    private void SelectFile()
     {
-        System.Random rd = new System.Random();
-        return rd.Next(11, 101);// tốc độ chậm nên chọn số bé
-    }
-    #endregion
-    #region so_Nguyento
-    private bool RSA_kiemTraNguyenTo(int xi)
-    {
-        bool kiemtra = true;
-        if (xi == 2 || xi == 3)
+        string filePath = UnityEditor.EditorUtility.OpenFilePanel("Select File", "", "");
+
+        if (!string.IsNullOrEmpty(filePath))
         {
-            // kiemtra = true;
-            return kiemtra;
-        }
-        else
-        {
-            if (xi == 1 || xi % 2 == 0 || xi % 3 == 0)
+            using (StreamReader reader = new StreamReader(filePath))
             {
-                kiemtra = false;
-            }
-            else
-            {
-                for (int i = 5; i <= Math.Sqrt(xi); i = i + 6)
-                    if (xi % i == 0 || xi % (i + 2) == 0)
-                    {
-                        kiemtra = false;
-                        break;
-                    }
+                string text = reader.ReadToEnd();
+                _ThongDiep_Input.text = text;
             }
         }
-        return kiemtra;
-    }
-    // "Hàm kiểm tra hai số nguyên tố cùng nhau"
-    private bool RSA_nguyenToCungNhau(int ai, int bi)
-    {
-        bool ktx_;
-        // giải thuật Euclid;
-        int temp;
-        while (bi != 0)
-        {
-            temp = ai % bi;
-            ai = bi;
-            bi = temp;
-        }
-        if (ai == 1) { ktx_ = true; }
-        else ktx_ = false;
-        return ktx_;
     }
     #endregion
-    #region so_mod
-    // "Hàm lấy mod"
-    public int RSA_mod(int mx, int ex, int nx)
+    #region ramdomkey
+   private void GenerateRandomKey()
+   {
+        key = new byte[16]; // Key length in bytes
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            rng.GetBytes(key);
+        }
+        string keyHex = BitConverter.ToString(key).Replace("-", string.Empty);
+        string keyString = Convert.ToBase64String(Encoding.UTF8.GetBytes(keyHex));
+        _Key_Input.text = keyString;
+    }
+    #endregion
+    #region EncryptFile
+    private void EncryptFile()
     {
+        if (_Key_Input.text == null || key.Length != 16)
+        {
+            _ThongDiep_Output.text = "Please generate a key first!";
+            return;
+        }
+        RC4 rc4 = new RC4(key);
+        byte[] encryptedData = rc4.Encrypt(fileData);
+        string keyString = ConvertByteArrayToString(encryptedData);
+        _ThongDiep_Output.text = keyString;
+    }
+    #endregion
+    #region DecryptFile
+    private void DecryptFile()
+    {
+        if (_Key_Input.text == null || key.Length != 16)
+        {
+            _ThongDiep_Output.text = "Please generate a key first!";
+            return;
+        }
+        RC4 rc4 = new RC4(key);
+        byte[] decryptedData = rc4.Decrypt(fileData);
+        string keyString = Encoding.UTF8.GetString(decryptedData);
+        _ThongDiep_Output.text = keyString;
+    }
+    #endregion
+    #region string to byte and byte to string
+    private string ConvertByteArrayToString(byte[] byteArray)
+    {
+        string hexString = Encoding.UTF8.GetString(byteArray);
+        return hexString;
+    }
 
-        //Sử dụng thuật toán "bình phương nhân"
-        //Chuyển e sang hệ nhị phân
-        int[] a = new int[100];
-        int k = 0;
-        do
-        {
-            a[k] = ex % 2;
-            k++;
-            ex = ex / 2;
-        }
-        while (ex != 0);
-        //Quá trình lấy dư
-        int kq = 1;
-        for (int i = k - 1; i >= 0; i--)
-        {
-            kq = (kq * kq) % nx;
-            if (a[i] == 1)
-                kq = (kq * mx) % nx;
-        }
-        return kq;
+    private byte[] ConvertToByteArray(string hexString)
+    {
+        byte[] byteArray = Encoding.UTF8.GetBytes(hexString);
+        return byteArray;
+    }
+    private bool IsValidHexString(string hexString)
+    {
+        Regex regex = new Regex("^[0-9A-F]+$");
+        return regex.IsMatch(hexString);
     }
     #endregion
-    #region tao_khoa
-    private void RSA_taoKhoa()
+    #region xacnhan input
+    private void OnConfirmButtonClick()
     {
-        //Tinh n=p*q
-        _RSA_N = _input_P * _input_Q;
-        _str_N = _RSA_N.ToString();
-        //Tính Phi(n)=(p-1)*(q-1)
-        _RSA_SoPhi_N = (_input_P - 1) * (_input_Q - 1);
-        _str_SoPhi_N = _RSA_SoPhi_N.ToString();
-        //Tính e là một số ngẫu nhiên có giá trị 1< e <phi(n) và là số nguyên tố cùng nhau với Phi(n)
-        do
-        {
-            System.Random RSA_rd = new System.Random();
-            _RSA_E = RSA_rd.Next(2, _RSA_SoPhi_N);
-        }
-        while (!nguyenToCungNhau(_RSA_E, _RSA_SoPhi_N));
-        _str_E = _RSA_E.ToString();
-
-        //Tính d là nghịch đảo modular của e
-        _RSA_D = 0;
-        int i = 2;
-        while (((1 + i * _RSA_SoPhi_N) % _RSA_E) != 0 || _RSA_D <= 0)
-        {
-            i++;
-            _RSA_D = (1 + i * _RSA_SoPhi_N) / _RSA_E;
-        }
-        _str_D = _RSA_D.ToString();
+        string keyString = _Key_Input.text;
+        key = ConvertToByteArray(keyString);
+        string DataString = _ThongDiep_Input.text;
+        fileData = ConvertToByteArray(DataString);
     }
     #endregion
-    public void Reset()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-    public void Quit()
-    {
-        Debug.Log("can");
-        Application.Quit();
-    }
 }
+#region RC4
+public class RC4
+{
+    private byte[] S;
+    private byte[] T;
+    private int keyLength;
+
+    public RC4(byte[] key)
+    {
+        keyLength = key.Length;
+        S = new byte[256];
+        T = new byte[256];
+
+        for (int i = 0; i < 256; i++)
+        {
+            S[i] = (byte)i;
+            T[i] = key[i % keyLength];
+        }
+
+        int j = 0;
+        for (int i = 0; i < 256; i++)
+        {
+            j = (j + S[i] + T[i]) % 256;
+            Swap(S, i, j);
+        }
+    }
+
+    public byte[] Encrypt(byte[] data)
+    {
+        byte[] encryptedData = new byte[data.Length];
+        int i = 0;
+        int j = 0;
+
+        for (int k = 0; k < data.Length; k++)
+        {
+            i = (i + 1) % 256;
+            j = (j + S[i]) % 256;
+
+            Swap(S, i, j);
+
+            int t = (S[i] + S[j]) % 256;
+
+            encryptedData[k] = (byte)(data[k] ^ S[t]);
+        }
+
+        return encryptedData;
+    }
+
+    public byte[] Decrypt(byte[] encryptedData)
+    {
+        // Decrypt the encrypted data using the RC4 algorithm
+        byte[] decryptedData = Encrypt(encryptedData);
+
+        // Convert the decrypted data back to a string using the correct encoding
+        string decryptedString = Encoding.UTF8.GetString(decryptedData);
+        return decryptedString;
+    }
+
+    private void Swap(byte[] array, int i, int j)
+    {
+        byte temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+}
+#endregion
